cbaadf6c985f1db4dc6ec142c76a4d3634979659
Sam Schams <samschams@gmail.com>
2024-11-22 22:19:49 +0100

    + validations

--- app/models/tracked_item.rb
+++ app/models/tracked_item.rb
@@ -1,6 +1,6 @@
 class TrackedItem < ApplicationRecord
-  # validates string not empty for item_name
   validates :item_name, presence: true, allow_blank: false
+  validate :is_hash, if: -> { price_overview_json.present? }
 
   def update_price_overview_json
     response = HTTParty.get(steam_market_price_overview_url)
@@ -26,6 +26,12 @@ class TrackedItem < ApplicationRecord
   end
 
   private
+
+  def is_hash
+    return if price_overview_json.is_a?(Hash)
+
+    errors.add(:price_overview_json, :invalid_data_type, message: "must be a hash")
+  end
 
   def uri_encoded_item_name
     URI::Parser.new.escape(item_name).gsub("&", "%26")
